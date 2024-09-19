e3addb9009cc09d1dfcf8cbfac5b19eb834fd01e
wedo <costadefelipe@gmail.com>
2024-09-19 20:46:17 -0300

    init

---
+++ lib/olxhub.ex
@@ -0,0 +1,18 @@
+defmodule Olxhub do
+  @moduledoc """
+  Documentation for `Olxhub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Olxhub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/olxhub_test.exs
@@ -0,0 +1,8 @@
+defmodule OlxhubTest do
+  use ExUnit.Case
+  doctest Olxhub
+
+  test "greets the world" do
+    assert Olxhub.hello() == :world
+  end
+end
